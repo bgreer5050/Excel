0903d967adb838d1047d19f33312ff98e10bffde
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-04-25 06:31:26 -0500

    Misc commit.  Successfully creates an excel file in the users document folder. NEXT - Read contents.

--- Excel/Program.cs
+++ Excel/Program.cs
@@ -1,15 +1,49 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Excel
+namespace Excel1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var ExcelObj = new Microsoft.Office.Interop.Excel.Application();
+            ExcelObj.Visible = false;
+
+            // Workbook wb = ExcelObj.Workbooks.Add()
+
+
+            try
+            {
+                Console.WriteLine(Environment.CurrentDirectory);
+                //ExcelObj.Workbooks.Open(Environment.CurrentDirectory.ToString() + "\this.xlsx");
+                Workbook wb =  ExcelObj.Workbooks.Add();
+                wb.Save();
+                Console.WriteLine(wb.FullName);
+                wb.SaveAs("test.xlsx");
+                Console.WriteLine(wb.FullName);
+
+
+                Console.WriteLine("Success");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Fail");
+
+            }
+            finally
+            {
+                
+            }
+            
+            while(true)
+            {
+
+            }
         }
     }
 }
